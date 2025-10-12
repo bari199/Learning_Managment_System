fc9f3d9c34085592bdebe75594fa6276f2719443
bari@199 <bari1999smab@gmail.com>
2025-10-12 10:11:45 -0700

    added Companies,CourseCard,CourseSection,Hero,Navbar,SearchBar,AppContext,pages,App,index,main jsx files and configure tailwind.css and added .env files

--- LMS/src/Components/student/CourseCard.jsx
+++ LMS/src/Components/student/CourseCard.jsx
@@ -1,7 +1,37 @@
 import React from "react";
+import { useContext } from "react";
+import { AppContext } from "../../Context/AppContext";
+import {assets} from "../../assets/assets"
+import { Link } from "react-router-dom";
+const CourseCard = ({ course }) => {
 
-const CourseCard = () => {
-  return <div>CourseCard</div>;
+  const {currency} = useContext(AppContext)
+
+  return (
+    <Link to={'/course/'+course._id} onClick={()=>scrollTo(0,0,)} 
+    className='border border-gray-500/30 pb-6 overflow-hidden rounded-lg'>
+      <img className="w-full" src={course.courseThumbnail} alt="" />
+      <div className="p-3 text-left" >
+        <h3 className="text-base font-semibold" >{course.courseTitle}</h3>
+        <p className="text-gray-500" >{course.educator.name}</p>
+        <div>
+          <p>4.5</p>
+          <div>
+            {[...Array(5)].map((_, i) => (
+              <img key={i} src={assets.star} alt="" />
+            ))}
+          </div>
+          <p>22</p>
+        </div>
+        <p>{currency}
+          {(
+            course.coursePrice -
+            (course.discount * course.coursePrice) / 100
+          ).toFixed(2)}
+        </p>
+      </div>
+    </Link>
+  );
 };
 
 export default CourseCard;
